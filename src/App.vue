99816b38797f3b1fb8094514436a6db865e52957
Ilya Shvedov <ilyashvedov5@gmail.com>
2022-12-17 16:58:37 +0300

    Finished styles of a table

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,54 @@
 <template>
   <div class="app">
     <main class="app__content">
-
+      <table-tools :tools="tools"/>
     </main>
   </div>
 </template>
+
+<script>
+import TableTools from '@/components/TableTools'
+
+export default {
+  components: {
+    TableTools
+  },
+  data () {
+    return {
+      tools: [
+        {
+          id: 1,
+          imgUrl: 'https://w7.pngwing.com/pngs/21/707/png-transparent-computer-icons-tool-others.png',
+          description: 'Молоток и ключ'
+        },
+        {
+          id: 2,
+          imgUrl: 'bbbbb',
+          description: 'Молоток и ключ'
+        },
+        {
+          id: 3,
+          imgUrl: 'zzzzzz',
+          description: 'Молоток ключ'
+        }
+      ]
+    }
+  }
+}
+</script>
+<style lang="scss">
+body {
+  background: #1E1E1E;
+}
+
+.app {
+  max-width: 998px;
+  padding: 5px;
+  margin: 0 auto;
+
+  &__content {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
---
+++ src/components/TableTools.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="table">
+    <div class="table__cell" v-for="i in 25" :key="i">
+      <div class="table__tool">
+        {{ getPath()[i - 1] }}
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    tools: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    getPath () {
+      return this.tools.map(tool => tool.description)
+    }
+  }
+}
+</script>
+<style scoped lang="scss">
+.table {
+  display: flex;
+  flex-wrap: wrap;
+  width: 585px;
+  min-height: 500px;
+  background: #262626;
+  border: 1px solid #4D4D4D;
+  border-radius: 12px;
+
+  &__cell {
+    padding: 5px;
+    border: 1px solid #4D4D4D;
+    width: 105px;
+    min-height: 100px;
+  }
+
+  &__cell:first-child {
+    border-radius: 12px 0 0 0;
+  }
+
+  &__cell:last-child {
+    border-radius: 0 0 12px 0;
+  }
+  &__cell:nth-child(5) {
+    border-radius: 0 12px 0 0;
+  }
+
+    &__cell:nth-child(21) {
+    border-radius: 0 0 0 12px;
+  }
+}
+</style>
